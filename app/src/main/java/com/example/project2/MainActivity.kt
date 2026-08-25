4e6a0e1bc9b53a8922275707f6982a4f167cc7fc
Aidan <aidanjsnyder@gmail.com>
2026-08-25 10:14:41 -0400

    TotalPerPersonText function added

--- app/src/main/java/com/example/project2/MainActivity.kt
+++ app/src/main/java/com/example/project2/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -18,11 +22,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.project2.ui.theme.Project2Theme
 
 class MainActivity : ComponentActivity() {
@@ -48,8 +56,38 @@ fun ProjectApp( modifier: Modifier = Modifier) {
 
 
 
-    Column(modifier = Modifier.padding(top = 50.dp)) {
+    Column(modifier = Modifier.padding(top = 50.dp, start = 14.dp, end = 14.dp)) {
+        TotalPerPersonText("Sample Text", Modifier
+            .background(
+                color = Color.LightGray,
+                shape = RoundedCornerShape(8.dp),
+            ).border(
+                width = 2.dp,
+                color = Color.Blue,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding(all = 82.dp)
+            .fillMaxWidth())
+
         TotalBillTextField(totalBillInput, {totalBillInput = it})
+    }
+}
+@Composable
+fun TotalPerPersonText(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier) {
+        // Text outputs
+        Text(
+            text = stringResource(R.string.total_per_person)
+        )
+        Text(text = text,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold)
     }
 }
 @Composable
